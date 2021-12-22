682c59eecc3b49656ae08016e5f92ca3daa4b89c
pkarthikeyan1312 <96464279+pkarthikeyan1312@users.noreply.github.com>
2021-12-22 12:04:20 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 * num2;
+
+      System.out.println("Multiply of these numbers: "+sum);
+   }
+}
